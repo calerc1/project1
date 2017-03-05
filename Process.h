952b53c3bfa7f276e58c03433392124ca95266e1
Austin Egri <Austin_Egri@theta-xi-103.dynamic2.rpi.edu>
2017-03-04 19:46:04 -0500

    Process.cpp

--- Process.h
+++ Process.h
@@ -3,9 +3,11 @@ class Process{
 public:
     Process();
     Process(string Id, int ArrivalTime, int BurstTime, int NumBurst, int IoTime);
+    Process(Process p2);
 	string id;
 	int arrivalTime;
 	int burstTime;
 	int numBurst;
 	int ioTime;
+	int burstRemain;
 };
